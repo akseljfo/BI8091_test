65dd79c0ee27f3c36f25044a1fa73812c8f2352d
Christianne Solvåg <chistii@hotmail.com>
2024-10-02 15:58:16 +0200

    what

--- test_repo.R
+++ test_repo.R
@@ -3,7 +3,11 @@
 x <- 1
 y <- 2
 
-x + y # Wow
+x + y # Wowwowowow
 
 # it could be helpful to describe what the variables x and y are standing for
-# Hola this is laura
+# Hola this is christianne
+
+print(x+y)
+
+# OK
